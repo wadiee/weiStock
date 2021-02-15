5c4f3a254d552ec199169cd4cf46dbb34c457dc4
wadiee <wei.zeng1993@gmail.com>
2021-02-14 21:17:55 -0800

    added homepage

--- src/main/java/com/example/WeiStock/StockController.java
+++ src/main/java/com/example/WeiStock/StockController.java
@@ -19,6 +19,7 @@ import com.fasterxml.jackson.databind.ObjectMapper;
 
 import org.json.JSONObject;
 import org.springframework.web.bind.annotation.ExceptionHandler;
+import org.springframework.web.bind.annotation.GetMapping;
 import org.springframework.web.bind.annotation.RequestMapping;
 import org.springframework.web.bind.annotation.RequestMethod;
 import org.springframework.web.bind.annotation.RequestParam;
@@ -58,6 +59,11 @@ public class StockController {
     private final int DaysToCrawl = 10;
 
     // private static final MongoConnection mongoConnection = new MongoConnection();
+
+    @GetMapping("/")
+    public String home() {
+        return "Welcomg to WeiStock";
+    }
 
     @RequestMapping(value = "/apiDoc", method = RequestMethod.GET)
     public Map<String, String> apiDocumentation() {
